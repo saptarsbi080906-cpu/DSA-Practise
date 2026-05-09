1e36554f9164c49f059690b3a03f0c1b03102b88
[Sapatarshi Sarkar] <saptarsbi080906@gmail.com>
2026-05-09 11:32:04 +0530

    adding more

--- patterns.cpp
+++ patterns.cpp
@@ -143,5 +143,57 @@ int main()
         cout << endl;
     */
 
+    /*---------------------End---------------------*/
+
+    /*
+    1
+    1 2
+    1 2 3
+    1 2 3 4 */
+
+    /*
+    int n;
+    cout << "Enter 'n' = ";
+    cin >> n;
+
+
+    for (int i = 0; i < n; i++)
+    {
+        int num = 1;
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }*/
+
+    /*---------------------End---------------------*/
+
+    /*
+    1 
+    2 1 
+    3 2 1 
+    4 3 2 1 */
+
+    /*
+    int n;
+    cout << "Enter 'n' = ";
+    cin >> n;
+
+
+    for (int i = 0; i < n; i++)
+    {
+        int num = 1;
+        num += i;
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << num << " ";
+            num--;
+        }
+        cout << endl;
+    }
+    */
+
     return 0;
 }
